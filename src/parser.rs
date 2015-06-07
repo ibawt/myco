5e1882f7e78124e31934d73f096803d3689c3288
Ian Quick <ian.quick@gmail.com>
2015-06-07 10:11:47 -0400

    string types, new parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,12 @@
-extern crate regex;
-
 use atom::*;
 use errors::*;
 
 use std::iter::*;
-use regex::Regex;
-
-// struct Scanner {
-//     buf: String,
-//     pos: usize
-// }
-
-// impl Scanner {
-//     fn next(&mut self) -> &str {
-//         ""
-//     }
-// }
+use std::io::prelude::*;
 
 pub fn tokenize(line: &str) -> ParseResult {
-    let r = Regex::new(r"\s+").unwrap();
-    let l = line.replace("(", " ( ").replace(")", " ) ").trim().to_string();
-    let iter = r.split(&l);
-    return read_tokens(&mut iter.peekable());
+    let mut chars = line.chars().peekable();
+    return read_tokens(&mut chars);
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -32,29 +17,172 @@ pub enum Node {
 
 pub type ParseResult = Result<Node, Error>;
 
-fn read_tokens(iter: &mut Peekable<regex::RegexSplits>) -> ParseResult {
-    match iter.next() {
-        Some("(") => {
+use std::str::Chars;
+
+#[derive (Debug, PartialEq)]
+enum Token {
+    Atom(Atom),
+    Open,
+    Close,
+    Quote,
+}
+
+use std::convert::From;
+
+
+impl Node {
+    fn symbol(s: &str) -> Node {
+        Node::Atom(Atom::Symbol(s.to_string()))
+    }
+
+    fn string(s: &str) -> Node {
+        Node::Atom(Atom::String(s.to_string()))
+    }
+
+    fn list(v: &[Atom]) -> Node {
+        use std::collections::*;
+        Node::Atom(Atom::List(v.iter().map(|x| x.clone()).collect::<VecDeque<Atom>>()))
+    }
+}
+
+impl From<i64> for Node {
+    fn from(i: i64) -> Node {
+        Node::Atom(Atom::Number(Number::Integer(i)))
+    }
+}
+
+impl From<f64> for Node {
+    fn from(f: f64) -> Node {
+        Node::Atom(Atom::Number(Number::Float(f)))
+    }
+}
+
+impl From<bool> for Node {
+    fn from(b: bool) -> Node {
+        Node::Atom(Atom::Boolean(b))
+    }
+}
+
+fn read_string(iter: &mut Peekable<Chars>) -> Result<Option<Token>, Error> {
+    let mut s = String::new();
+    loop {
+        match iter.next() {
+            Some('\\') => {
+                if let Some(escaped) = iter.next() {
+                    s.push(escaped);
+                } else {
+                    return Err(Error::Parser)
+                }
+            },
+            Some('"') => {
+                let t = Token::Atom(Atom::String(s.clone()));
+
+                return Ok(Some(t))
+            },
+            Some(c) => s.push(c),
+            None => return Err(Error::Parser)
+        }
+    }
+}
+
+fn read_atom(c: char, iter: &mut Peekable<Chars>) -> Result<Option<Token>,Error> {
+    let mut s = String::new();
+    s.push(c);
+    loop {
+        match iter.peek() {
+            Some(&')') =>  {
+                return Ok(Some(Token::Atom(Atom::parse(&s))))
+            },
+            _ => ()
+        }
+
+        if let Some(c) = iter.next() {
+            if !c.is_whitespace() {
+                s.push(c);
+            } else {
+                return Ok(Some(Token::Atom(Atom::parse(&s))))
+            }
+        } else {
+            return Ok(Some(Token::Atom(Atom::parse(&s))))
+        }
+    }
+}
+
+fn next(iter: &mut Peekable<Chars>) -> Result<Option<Token>, Error> {
+    loop {
+        if let Some(c) = iter.next() {
+            match c {
+                '(' => return Ok(Some(Token::Open)),
+                '"' => return read_string(iter),
+                ')' => return Ok(Some(Token::Close)),
+                '\'' => return Ok(Some(Token::Quote)),
+                ';' =>  {
+                    loop {
+                        match iter.next() {
+                            Some('\n') => break,
+                            Some(_) => (),
+                            None => return Ok(None)
+                        }
+                    }
+                },
+                _ if c.is_whitespace() => (),
+                _ => return read_atom(c, iter)
+            }
+        } else {
+            return Ok(None)
+        }
+    }
+}
+
+fn read_tokens(chars: &mut Peekable<Chars>) -> ParseResult {
+    match next(chars) {
+        Ok(Some(Token::Open)) => {
             let mut node: Vec<Node> = vec![];
 
             loop {
-                match iter.peek() {
-                    Some(&")") => {
-                        iter.next();
+                match chars.peek() {
+                    Some(&')') => {
+                        chars.next();
                         break
                     },
                     Some(_) => {
-                        let token = try!(read_tokens(iter));
+                        let token = try!(read_tokens(chars));
                         node.push(token);
                     },
-                    None => return Err(Error::Parser)
+                    _ => {
+                        return Err(Error::Parser)
+                    }
                 }
             }
-            return Ok(Node::List(node));
+
+            return Ok(Node::List(node))
         },
-        Some(")") => return Err(Error::Parser),
-        Some(x) => return Ok(Node::Atom(Atom::parse(x))),
+        Ok(Some(Token::Close)) => {
+            return Err(Error::Parser)
+        },
+        Ok(Some(Token::Atom(x))) => return Ok(Node::Atom(x)),
         _ => ()
     }
-    return Err(Error::Parser);
+    Err(Error::Parser)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn naked_atoms() {
+        assert_eq!(Node::from(0), tokenize("0").unwrap());
+        assert_eq!(Node::from(512), tokenize("512").unwrap());
+        assert_eq!(Node::from(-512), tokenize("-512").unwrap());
+        assert_eq!(Node::from(5.0f64), tokenize("5.0").unwrap());
+        assert_eq!(Node::string("foo bar"), tokenize("\"foo bar\"").unwrap());
+        assert_eq!(Node::symbol("foo"), tokenize("foo").unwrap());
+    }
+
+    #[test]
+    fn string_escaping() {
+        assert_eq!(Node::string("foo'bar"), tokenize("\"foo\\'bar\"").unwrap());
+        assert_eq!(Node::string("foo\"bar"), tokenize("\"foo\\\"bar\"").unwrap());
+    }
 }
