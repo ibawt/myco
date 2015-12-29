20e1bbf4e40a4a20dfb589295722e1dc3e163bac
Ian Quick <ian.quick@gmail.com>
2015-12-29 14:19:58 -0500

    interner?

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,21 +1,66 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::rc::Rc;
+use std::cell::RefCell;
+use std::ops::Deref;
 
-lazy_static! {
-    static ref SYMBOLS: HashSet<Rc<String>> = {
-        HashSet::new()
-    };
+#[derive (Eq, PartialEq, Clone, Copy, Default, Hash, Debug)]
+pub struct InternedStr(usize);
+
+#[derive (Debug)]
+pub struct Interner {
+    indices: HashMap<String, usize>,
+    strings: Vec<String>
 }
 
-pub fn contains(n: &str) -> bool {
-    SYMBOLS.contains(n)
-}
-
-
-pub fn fetch(n: &str) -> Rc<String> {
-    if !SYMBOLS.contains(n) {
-        SYMBOLS.insert(n.to_owned());
+impl Interner {
+    pub fn new() -> Interner {
+        Interner { indices: HashMap::new(), strings: vec![] }
     }
 
-    &SYMBOLS.get(n).unwrap()
+    pub fn intern(&mut self, s: &str) -> InternedStr {
+        match self.indices.get(s).map(|x| *x) {
+            Some(index) => InternedStr(index),
+            None => {
+                let index = self.strings.len();
+                self.indices.insert(s.to_string(), index);
+                self.strings.push(s.to_string());
+                InternedStr(index)
+            }
+        }
+    }
+
+    pub fn get_str(&self, InternedStr(i): InternedStr) -> &str {
+        if i < self.strings.len() {
+            &*self.strings[i]
+        } else {
+            panic!("argh!");
+        }
+    }
+}
+
+pub fn get_local_interner() -> Rc<RefCell<Interner>> {
+    thread_local!( static INTERNER: Rc<RefCell<Interner>> = Rc::new(RefCell::new(Interner::new())));
+    INTERNER.with(|interner| interner.clone())
+}
+
+pub fn intern(s: &str) -> InternedStr {
+    let interner = get_local_interner();
+    let mut i = interner.borrow_mut();
+    i.intern(s)
+}
+
+impl Deref for InternedStr {
+    type Target = str;
+    fn deref(&self) -> &str {
+        self.as_ref()
+    }
+}
+
+impl AsRef<str> for InternedStr {
+    fn as_ref(&self) -> &str {
+        let interner = get_local_interner();
+        let x = (*interner).borrow_mut();
+        let r: &str = x.get_str(*self);
+        unsafe { ::std::mem::transmute(r) }
+    }
 }
