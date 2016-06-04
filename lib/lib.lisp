3dfdf5ebc6cc652dceb6a572b86c63b49b18cdfa
Ian Quick <ian.quick@gmail.com>
2016-06-04 18:06:33 -0400

    Compile functions (#12)
    * functions can compile now
    
    * fix map* and add test

--- lib/lib.lisp
+++ lib/lib.lisp
@@ -44,7 +44,7 @@
        ~@body)))
 
 (defun second (x)
-  (first (first x)))
+ (first (rest x)))
 
 (defmacro cond (& xs)
   (if (> (count xs) 0)
@@ -52,5 +52,11 @@
             (second (first xs))
             (cons 'cond (rest xs)))))
 
-(defun empty? (x)
-  (= 0 (count x)))
+(defmacro empty? (x)
+  `(= 0 (count ~x)))
+
+(defun map* (f coll out)
+  (if (empty? coll)
+      out
+      (recur f (rest coll)
+             (append (f (first coll)) out))))
--- lib/lib2.lisp
+++
@@ -1,5 +0,0 @@
-(defun map* (f coll out)
-  (if (empty? coll)
-      out
-      (recur f (rest coll)
-             (append (f (first coll)) out))))
